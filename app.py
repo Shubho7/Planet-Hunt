db02c2cb277d5975f937632e571a9257f30d70e8
Shubho7 <banerjeesubhodeep54@gmail.com>
2024-09-11 01:21:17 +0530

    styling changes

--- app.py
+++ app.py
@@ -12,15 +12,39 @@ scaler = joblib.load('C:/Users/baner/Documents/Planet-Hunt/notebooks/scaler.pkl'
 # Streamlit app UI
 st.title('Exoplanet Habitability')
 
-# Creating input fields for the 10 features
-P_MASS = st.number_input('Mass of the planet')
-P_RADIUS = st.number_input('Planetary Radius of the planet')
-P_PERIOD = st.number_input('Orbital Period of the planet')
-P_SEMI_MAJOR_AXIS = st.number_input('Semi-major Axis of the planet')
-P_ECCENTRICITY = st.number_input('Orbital Eccentricity of the planet')
-P_INCLINATION = st.number_input('Orbital Inclination of the planet')
-P_ESCAPE = st.number_input('Escape Velocity of the planet')
-P_POTENTIAL = st.number_input('Gravitational Potential of the planet')
+# Organizing input fields into two columns
+col1, col2 = st.columns(2)
+
+# Column 1 input fields
+with col1:
+    P_MASS = st.number_input('Mass of the planet')
+    P_RADIUS = st.number_input('Planetary Radius of the planet')
+    P_PERIOD = st.number_input('Orbital Period of the planet')
+    P_SEMI_MAJOR_AXIS = st.number_input('Semi-major Axis of the planet')
+
+# Column 2 input fields
+with col2:
+   P_ECCENTRICITY = st.number_input('Orbital Eccentricity of the planet')
+   P_INCLINATION = st.number_input('Orbital Inclination of the planet')
+   P_ESCAPE = st.number_input('Escape Velocity of the planet')
+   P_POTENTIAL = st.number_input('Gravitational Potential of the planet')
+
+# Styling the button using st.markdown and HTML
+st.markdown("""
+    <style>
+    div.stButton > button:first-child {
+        background-color: #4CAF50;
+        color:#ffffff;
+        border-radius:10px;
+        height:50px;
+        width:200px;
+        font-size:18px;
+    }
+    div.stButton > button:first-child:hover {
+        background-color: #45a049;
+        color:#ffffff;
+    }
+    </style>""", unsafe_allow_html=True)
 
 # When the user clicks the Predict button
 if st.button('Predict'):
