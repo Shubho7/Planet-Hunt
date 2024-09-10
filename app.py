fcd28d119ddaa75fd83e2a060f02e3b1c5749897
Shubho7 <banerjeesubhodeep54@gmail.com>
2024-09-11 01:08:23 +0530

    more changes inb app.py

--- app.py
+++ app.py
@@ -13,16 +13,14 @@ scaler = joblib.load('C:/Users/baner/Documents/Planet-Hunt/notebooks/scaler.pkl'
 st.title('Exoplanet Habitability')
 
 # Creating input fields for the 10 features
-P_MASS = st.number_input('Planetary Mass (P_MASS)', value=1.0)
-P_RADIUS = st.number_input('Planetary Radius (P_RADIUS)', value=1.0)
-P_PERIOD = st.number_input('Orbital Period (P_PERIOD)', value=1.0)
-P_SEMI_MAJOR_AXIS = st.number_input('Semi-major Axis (P_SEMI_MAJOR_AXIS)', value=1.0)
-P_ECCENTRICITY = st.number_input('Orbital Eccentricity (P_ECCENTRICITY)', value=0.0)
-P_INCLINATION = st.number_input('Orbital Inclination (P_INCLINATION)', value=0.0)
-P_ESCAPE = st.number_input('Escape Velocity (P_ESCAPE)', value=0.0)
-P_POTENTIAL = st.number_input('Gravitational Potential (P_POTENTIAL)', value=0.0)
-P_GRAVITY = st.number_input('Surface Gravity (P_GRAVITY)', value=0.0)
-P_DENSITY = st.number_input('Density (P_DENSITY)', value=1.0)
+P_MASS = st.number_input('Mass of the planet')
+P_RADIUS = st.number_input('Planetary Radius of the planet')
+P_PERIOD = st.number_input('Orbital Period of the planet')
+P_SEMI_MAJOR_AXIS = st.number_input('Semi-major Axis of the planet')
+P_ECCENTRICITY = st.number_input('Orbital Eccentricity of the planet')
+P_INCLINATION = st.number_input('Orbital Inclination of the planet')
+P_ESCAPE = st.number_input('Escape Velocity of the planet')
+P_POTENTIAL = st.number_input('Gravitational Potential of the planet')
 
 # When the user clicks the Predict button
 if st.button('Predict'):
