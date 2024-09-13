9007ae992d67d68ec5704237de6532780776e8e0
Shubho7 <banerjeesubhodeep54@gmail.com>
2024-09-14 01:34:56 +0530

    ui changes

--- app.py
+++ app.py
@@ -10,7 +10,25 @@ model = tf.keras.models.load_model('C:/Users/baner/Documents/Planet-Hunt/noteboo
 scaler = joblib.load('C:/Users/baner/Documents/Planet-Hunt/notebooks/scaler.pkl') 
 
 # Streamlit app UI
-st.title('Exoplanet Habitability')
+# Tile
+st.markdown("""
+    <h2 style="text-align: center; 
+            color: white;">
+            <b>PLANET HUNT</b></h2>
+""", unsafe_allow_html=True)
+
+# Description
+st.markdown("""
+    <div style="padding:15px;">
+        <p style="font-style: Arial; 
+            font-size:18px; 
+            color:#ffffff; 
+            text-align: justify;">
+            Welcome to <b>Planet Hunt</b>🪐✨ an exoplanet habitability predictor<br>
+            Imagine looking up at the night sky and knowing that out there, beyond the twinkling stars, lie countless hidden worlds—planets with wild landscapes, alien weather, and maybe, just maybe, the right ingredients for life. These are Exoplanets, and we’re on a mission to uncover their secrets. Some may be scorching hot giants, others frozen, but a few could be habitable oases in the vast cosmic desert drifting in the vast emptiness of space. Every time we spot a new exoplanet, it feels like opening a door to a new sci-fi story. Some could be potential homes for life, with alien sunsets we can only dream of. Who knows, the next big discovery might just reveal a second Earth?, or a world so strange it redefines our imagination. Let’s find out together!
+        </p>
+    </div>
+""", unsafe_allow_html=True)
 
 # Organizing input fields into two columns
 col1, col2 = st.columns(2)
@@ -61,23 +79,41 @@ if st.button('PREDICT'):
 
     # Displaying the prediction
     if prediction == 0:
-        # Uninhabitable - Red background
+        # Uninhabitable
         st.markdown(f"""
-            <div style="background-color:#f44336;padding-left:10px;padding-top:12px;border-radius:10px;display:inline-block;">
-            <h5 style="color:white;text-align:left;">The planet is Uninhabitable</h5>
+            <div style="background-color:#f20000;
+                padding-left:10px;
+                padding-top:12px;
+                border-radius:10px;
+                display:inline-block;">
+                <h5 style="color:white;
+                text-align:left;">
+                Oh no! you might have found an <b>Uninhabitable Planet</b></h5>
             </div>
         """, unsafe_allow_html=True)
     elif prediction == 1:
-        # Conservatively Habitable - Green background
+        # Conservatively Habitable
         st.markdown(f"""
-            <div style="background-color:#00D032;padding-left:10px;padding-top:12px;border-radius:10px;display:inline-block;">
-            <h5 style="color:white;text-align:left;">The planet is Conservatively Habitable</h5>
+            <div style="background-color:#00D032;
+                padding-left:10px;
+                padding-top:12px;
+                border-radius:10px;
+                display:inline-block;">
+                <h5 style="color:white;
+                text-align:left;">
+                Whoa! you might have just found a <b>Conservatively Habitable Planet</b></h5>
             </div>
         """, unsafe_allow_html=True)
     else:
-        # Optimistically Habitable - Yellow background
+        # Optimistically Habitable
         st.markdown(f"""
-            <div style="background-color:#FFAF0B;padding-left:10px;padding-top:12px;border-radius:10px;display:inline-block;">
-            <h5 style="color:black;text-align:left;">The planet is Optimistically Habitable</h5>
+            <div style="background-color:#FFAF0B;
+                padding-left:10px;
+                padding-top:12px;
+                border-radius:10px;
+                display:inline-block;">
+                <h5 style="color:black;
+                text-align:left;">
+                Keep going! you might have found a <b>Optimistically Habitable Planet</b></h5>
             </div>
         """, unsafe_allow_html=True)
